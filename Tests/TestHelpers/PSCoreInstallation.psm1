cc5c94475e5765d5fc809e6b75d101592f26869f
Norberto Arrieta <narrieta@microsoft.com>
2017-07-14 10:29:17 -0700

    Fix uri

--- Tests/TestHelpers/PSCoreInstallation.psm1
+++ Tests/TestHelpers/PSCoreInstallation.psm1
@@ -2,27 +2,20 @@
 $errorActionPreference = 'Stop'
 
 
-function Get-LastUriSegment
-{
-  param (
-    [Parameter(Mandatory=$true,Position=1)]
-    [Uri] $Uri
-  )
-    
-  if ($Uri.Segments.Count -lt 1)
-  {
-    throw "Invalid URL: $url"
-  }
-
-  return $Uri.Segments[-1]
-}
 function Get-Download {
   param(
     [Parameter(Mandatory=$true,Position=1)]
     [Uri] $Uri
   )
 
-  $downloadLocation = Join-Path ([System.IO.Path]::GetTempPath()) (Get-LastUriSegment $Uri)
+  if ($Uri.Segments.Count -lt 1)
+  {
+    throw "Invalid URL: $url"
+  }
+
+  $fileName = $Uri.Segments[-1]
+
+  $downloadLocation = Join-Path ([System.IO.Path]::GetTempPath()) $fileName
     
   if (Test-Path $downloadLocation) {
     Remove-Item -Force $downloadLocation
@@ -33,34 +26,20 @@ function Get-Download {
   return $downloadLocation
 }
 
-function Wait-Process{
+function Invoke-Installer {
   param (
     [Parameter(Mandatory=$true,Position=1)]
-    [System.Diagnostics.Process] $Process
+    [string] $Installer
   )
 
-}
-
-function Install-Download {
-  param (
-    [Parameter(Mandatory=$true,Position=1)]
-    [Uri] $Uri
-  )
-
-  $downloadLocation = Get-Download $Uri
-
-  Write-Verbose "Installing $downloadLocation..."
-
-  $process = [System.Diagnostics.Process]::Start($downloadLocation, "/quiet /norestart ")
+  $process = [System.Diagnostics.Process]::Start($Installer, "/quiet /norestart ")
   
     if (-not $process.WaitForExit(10 * 60 * 1000))
     {
-        throw "Installing $($Process.Name) timed out"
+        throw "Timeout installing $Installer"
     }
 
    $returnCode = $Process.ExitCode
-
-   Write-Verbose "Install return code: $returnCode"
 
    return $returnCode
 }
@@ -69,14 +48,16 @@ function Install-PowerShellCore
 {
     [CmdletBinding()]
     param ()
-
     #
     # PS Core installers:
     #   * https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.4/PowerShell-6.0.0-beta.4-win10-win2016-x64.msi
-    #   * https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.4/PowerShell-6.0.0-beta.4-win81-win2012r2-x64.zip
+    #   * https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.4/PowerShell-6.0.0-beta.4-win81-win2012r2-x64.msi
+    #
     # The standard Appveyor image is a Windows Server 2012 R2, which is installed below
     #
-    Install-Download 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.4/PowerShell-6.0.0-beta.4-win81-win2012r2-x64.zip'
+    $installer = Get-Download 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.4/PowerShell-6.0.0-beta.4-win81-win2012r2-x64.msi'
+
+    Invoke-Installer $installer
 }
 
 Export-ModuleMember -Function Install-PowerShellCore
